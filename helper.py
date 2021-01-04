5a1ce7525f109887ac393c3eeca123de55f63990
LaRena Iocco <rosie.mcqueen@gmail.com>
2021-01-04 18:09:44 -0500

    New user registration working

--- helper.py
+++ helper.py
@@ -1,4 +1,4 @@
-""" Database query functions"""
+""" Helper functions"""
 
 from model import (db, User, Book, connect_to_db)
 # import json
@@ -8,6 +8,14 @@ def get_user_by_email(email):
     """Look up user by email."""
 
     return User.query.filter(User.email == email).first()
+
+def get_length_of_phone(phone):
+    
+    if len(phone) == 10:
+        return True
+    else:
+        return False
+
 
 if __name__ == '__main__':
     from server import app
--- server.py
+++ server.py
@@ -25,9 +25,7 @@ def homepage():
 def login_user():
     """ Login user """
 
-    print('login route')
     email = request.form['email']
-    print(email)
     incoming_password = request.form['password']
     user = helper.get_user_by_email(email)
     if user == None:
@@ -42,7 +40,30 @@ def login_user():
         else:
             flash('Incorrect Password. Please try again.')
             return redirect ('/') 
-    # return render_template('profile.html') 
+
+@app.route('/create-user', methods=['POST'])
+def create_user():
+    """Create new user account"""
+    email = request.form['email']
+    password = request.form['password']
+    fname = request.form['fname']
+    lname = request.form['lname']
+    phone = request.form['phone']
+    user = helper.get_user_by_email(email)
+
+    if len(phone) != 10:
+        flash('Phone numbers must be 10 digits long')
+        return redirect ('/')
+    phone = '+1' + phone
+    print(phone)
+
+    if user != None:
+        flash('This email is already associated with an account. Please log in.')
+        return redirect ('/')
+    else:
+        crud.create_user(email, password, fname, lname, phone)
+        flash('Your account has been created.  Please log in.')
+        return redirect ('/')
 
 
 
