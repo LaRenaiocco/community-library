bb1054599e60739ece9f661be1f6b9394564fa44
LaRena Iocco <rosie.mcqueen@gmail.com>
2021-01-08 20:07:29 -0500

    Genre input parsed and added to formData in JS

--- static/js/profile.js
+++ static/js/profile.js
@@ -3,13 +3,16 @@
 $('#upload-image-form').on('submit', (evt) => {
     evt.preventDefault();
     console.log('default prevented')
-    // console.log($('#author-field').val())
 
+    const selectedGenres = $('#checkboxes input:checked').map(function(i,el){return el.name;}).get();
+
+    console.log(selectedGenres)
 
     const formData = new FormData();
     formData.append('file', $('#image-field').prop('files')[0]);
     formData.append('title', $('#title-field').val());
     formData.append('author', $('#author-field').val());
+    formData.append('genres', selectedGenres)
     console.log(formData)
 
     $.ajax({
