753d9ff74c86b121f02ff8e6ab683d79b18f8805
LaRena Iocco <rosie.mcqueen@gmail.com>
2021-01-14 18:31:51 -0500

    Testing for crud functions create user, create book, delete book

--- test.py
+++ test.py
@@ -4,7 +4,7 @@
 
 from unittest import TestCase
 from flask import session
-from model import connect_to_db, db, example_data
+from model import (connect_to_db, db, example_data, User, Book)
 from server import app
 import helper
 import crud
@@ -14,7 +14,7 @@ class FlaskTestsBasic(TestCase):
     """Flask route path tests (no db)"""
 
     def setUp(self):
-        """To do before every test."""
+        """To do before every test"""
 
         self.client = app.test_client()
         app.config['TESTING'] = True
@@ -41,20 +41,49 @@ class CrudAndHelperTests(TestCase):
     """Crud and Helper function tests (with db)"""
 
     def setUp(self):
-    """To do before every test."""
+        """To do before every test"""
 
-    self.client = app.test_client()
-    app.config['TESTING'] = True
-    connect_to_db(app, "postgresql:///apptestdb")
-    db.create_all()
-    example_data()
+        self.client = app.test_client()
+        app.config['TESTING'] = True
+        connect_to_db(app, "postgresql:///apptestdb")
+        db.create_all()
+        example_data()
 
     def tearDown(self):
-    """Do at end of every test."""
+        """To do at end of every test"""
 
-    db.session.remove()
-    db.drop_all()
-    db.engine.dispose()
+        db.session.remove()
+        db.drop_all()
+        db.engine.dispose()
+
+    def test_create_user(self):
+        """Tests creation of user in database"""
+
+        u = crud.create_user(email='Claire@claire.com', 
+                            password='test', 
+                            fname='Claire', 
+                            lname='Cramer', 
+                            phone='1234567890')
+        self.assertIsNotNone(u.user_id)
+
+    def test_create_book(self):
+        """Tests creation of user in database"""
+
+        b = crud.create_book(title='Tipping the Velvet', 
+                            author='Sarah Waters', 
+                            genre='fiction, historical fiction, LGBT', 
+                            description=None, 
+                            image_url='https://res.cloudinary.com/rosieslibrary/image/upload/v1609811721/Books/tipping_the_velvet_kmsojp.jpg',
+                            owner=1,
+                            available=True)
+        self.assertIsNotNone(b.book_id)
+
+    def test_delete_book(self):
+        """Tests deleting of book from database"""
+
+        book_id = 1
+        crud.delete_book(book_id)
+        self.assertFalse(Book.query.get(book_id))
 
 
 if __name__ == '__main__':
